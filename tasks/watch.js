c2db439a804ca1076cc5ac7e6406d29f935300b2
André Filipe <afgoulart.rj@gmail.com>
2016-05-12 12:03:53 -0300

    webpackk hot reload

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,14 +4,24 @@ import gulp from 'gulp';
 import gls from 'gulp-live-server';
 
 module.exports = () => {
-  const server = gls.new('server/app.babel.js', {}, true);
+  const server = gls.new('server/app.babel.js', {
+    env: {
+      NODE_ENV: process.env.NODE_ENV || 'development'
+    }
+  });
   server.start();
 
-  const notify = (event) => server.notify.apply(server, [event.path]);
+  const notify = (file) => {
+    console.log(file);
+    server.notify.apply(server, [file]);
+  };
 
-  gulp.watch(['./template.js']).on('change', notify);
-  gulp.watch(['assets/sass/**/*.scss'], ['sass']).on('change', notify);
-  gulp.watch(['assets/svg/**/*.scss'], ['svgstore']).on('change', notify);
+  gulp.watch(['./template.js'], notify);
+  gulp.watch(['assets/sass/**/*.scss'], ['sass']);
+  gulp.watch(['assets/svg/**/*.svg'], ['svgstore']);
+
+  gulp.watch(['dist/**/*.css', 'dist/**/*.img', 'dist/**/*.html'], notify);
+
 
   gulp.watch(['assets/img/**/*'], ['img']).on('change', notify);
   gulp.watch(['server/**/*.js'], server.start.bind(server));
