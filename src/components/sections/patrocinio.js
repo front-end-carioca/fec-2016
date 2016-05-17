186471f31df9db50de5ae863a2a9db4c33e6c71b
André Filipe <afgoulart.rj@gmail.com>
2016-05-16 23:26:41 -0300

    add patrocinadores

--- src/components/sections/patrocinio.js
+++ src/components/sections/patrocinio.js
@@ -9,15 +9,43 @@ class Patrocinio extends Component {
   }
 
   render() {
-    console.log('props', this.props)
     const {name, patrocinadores, children} = this.props;
-    console.log('patrocinadores', patrocinadores);
     return (
-      <section className="section" id={name}>
+      <section className="section patrocinadores" id={name}>
+        <h2 className="section-title">PATROCINADORES</h2>
         <Grid>
           <Row>
             <Col xs={12}>
-              {patrocinadores.Platinum.map(p => <h2>{p}</h2>)}
+              <h2 className="status">Platinum</h2>
+              {patrocinadores.Platinum.map(p => <img src={`/img/patrocinadores/${p.image}`} title={p.name} />)}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <h2 className="status">Vip</h2>
+              {patrocinadores.Vip.map(p => <img src={`/img/patrocinadores/${p.image}`} title={p.name} />)}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={4}>
+              <h2 className="status">Ouro</h2>
+              {patrocinadores.Ouro.map(p => <img src={`/img/patrocinadores/${p.image}`} title={p.name} />)}
+            </Col>
+            <Col xs={4}>
+              <h2 className="status">Prata</h2>
+              {patrocinadores.Prata.map(p => <img src={`/img/patrocinadores/${p.image}`} title={p.name} />)}
+            </Col>
+            <Col xs={4}>
+              <h2 className="status">Bronze</h2>
+              {patrocinadores.Bronze.map(p => <img src={`/img/patrocinadores/${p.image}`} title={p.name} />)}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <h2 className="status">Apoi</h2>
+              <Row>
+                {patrocinadores.Apoio.map(p => <Col xs={12} sm={4}><img src={`/img/patrocinadores/${p.image}`} title={p.name} /></Col>)}
+              </Row>
             </Col>
           </Row>
         </Grid>
