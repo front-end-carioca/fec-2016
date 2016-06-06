850a56d23c88f12865066ee6662092a6133d816d
André Filipe <afgoulart.rj@gmail.com>
2016-06-06 12:21:54 -0300

    change layout section 'sobre'

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -16,7 +16,7 @@ class Sobre extends Component {
           <Row>
             <Col xs={12}>
               <div className="content">
-                <h2 className="title">SOBRE</h2>
+                <h2 className="section-title">SOBRE</h2>
                 <p>O Front-end Carioca Surgiu de uma idéia dentro da comunidade de desenvolvedores web, tendo como obijetivo enriquecer o conhecimento profissional através de ciclos de palestras, workshops, networking e é claro bastante incentivo.</p>
                 <p>Em 2014, na Universidade Veiga de Almeida (UVA), realizamos nossa primeira edição e com certeza foi um sucesso, fizemos de tudo para apresentar um ótimo conteúdo aos participantes. Já em 2016 estamos vindo com força total e bastante conteúdo legal, propostas de estágios, empregos, parcerias, sorteios de cursos e de livros. Além da principal parte do evento: networking, onde os participantes trocam idéias/experiências e a comunidade cresce.</p>
               </div>
